4607bff40a6454ba113c8311fccc8790fddc27a6
Christopher Gioia <christophergioia64@gmail.com>
2024-03-06 07:06:58 -0800

    fix wishlist.service.ts

--- ecommerce-prototype/src/app/wishlist.service.ts
+++ ecommerce-prototype/src/app/wishlist.service.ts
@@ -2,7 +2,6 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { AuthServiceService } from './auth-service.service';
 import { Product } from './dto/product';
-import { UrlLibService } from './url-lib.service';
 
 @Injectable({
   providedIn: 'root'
@@ -13,7 +12,7 @@ export class WishlistService {
   private deleteWishlistItemUrl= "http://localhost:8080/wishlist/removeitem"
   private postWishlistItemUrl="http://localhost:8080/wishlist/additem"
 
-  constructor(private http:HttpClient,private authService:AuthServiceService, private urldir:UrlLibService) { }
+  constructor(private http:HttpClient,private authService:AuthServiceService) { }
 
   getWishlist(){
     const token = this.authService.getToken();
