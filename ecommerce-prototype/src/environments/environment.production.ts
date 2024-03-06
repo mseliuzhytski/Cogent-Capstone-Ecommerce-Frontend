0118e08a42fbb48673de783d36d0179c0fea0d98
Rafi <rafirafi9550@gmail.com>
2024-03-06 17:55:34 -0500

    port slashes added

--- ecommerce-prototype/src/environments/environment.production.ts
+++ ecommerce-prototype/src/environments/environment.production.ts
@@ -1,6 +1,6 @@
 export const environment = {
   environmentName: "Production",
   url: "http://localhost:9005/",
-  sales_url: "http://localhost:9005",
-  discount_url: "http://localhost:9005"
+  sales_url: "http://localhost:9005/",
+  discount_url: "http://localhost:9005/"
 };
